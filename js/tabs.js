57367be2d1a1757a9c5e50cf8c459fcb87c8457f
thebuilderekes <ekeopreberdugo1@gmail.com>
2024-01-23 14:30:14 +0100

    added apartments section

---
+++ js/tabs.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const selectElement = document.getElementById('inputGroupSelect01');
+    const tabs = document.querySelectorAll('.tab-link');
+    const tabContents = document.querySelectorAll('.tab-content');
+   
+    selectElement.addEventListener('change', function() {
+      const selectedTabIndex = parseInt(this.value) - 1; // Adjust for zero-based indexing
+   
+      // Deactivate all tabs and contents
+      tabs.forEach(tab => tab.classList.remove('current'));
+      tabContents.forEach(content => content.classList.remove('current'));
+   
+      // Activate the selected tab and content
+      tabs[selectedTabIndex].classList.add('current');
+      tabContents[selectedTabIndex].classList.add('current');
+    });
+   });
+   
+//  
