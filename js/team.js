b8a265f8808cf44b5b1ff559a167a433455df98a
thebuilderekes <ekeopreberdugo1@gmail.com>
2024-03-04 14:28:54 +0100

    modified pricing page

---
+++ js/team.js
@@ -0,0 +1,24 @@
+// const teamMemberImages = document.querySelectorAll('.team-member');
+// const teamMemberArticle = document.querySelectorAll('.team-member__article');
+
+// teamMemberImages.forEach(memberImage => {
+//      memberImage.addEventListener('mouseover', () => {
+//        teamMemberArticle.style.display = "block";
+//       });
+//   })
+// teamMemberImages.addEventListener("mouseover", function(){
+// })
+
+// const teamMemberImages = document.querySelectorAll('.team-member');
+
+// teamMemberImages.forEach(image => {
+//   image.addEventListener('mouseover', () => {
+//     const siblingArticle = image.nextElementSibling;
+//     siblingArticle.style.display = 'block';
+//   });
+
+//   image.addEventListener('mouseout', () => {
+//     const siblingArticle = image.nextElementSibling;
+//     siblingArticle.style.display = 'none';
+//   });
+// });
